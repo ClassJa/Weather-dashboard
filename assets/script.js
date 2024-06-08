8453ab1d912a8cf95776538db43653fc3ad59d41
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-08 09:34:41 -0500

    Debugging js fetch request

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,18 @@ const apiKey = '18e7ae3a1f608e812d465476189f028c'
 
 const baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`
 
-function retrieveApiDetails() {
-    fetch(baseUrl) {
-        if(!response.ok) {
-            alert(`Url is not working, response: ${response}`)
-        }
-    }
-}
-retrieveApiDetails()
+
+// if (baseUrl.ok) {
+    fetch(baseUrl)
+    .then(resonse)
+    console.log(response)
+    .catch(`Url is not working, response: ${baseUrl.status}`)
+// }
+
+// .then(data) {
+//     console.log(JSON.parse(data))
+// }
+
+//  Handle the response correctly by using the .then() and .catch() methods.
+// 2. Parse the JSON response.
+// 3. Improve error handling.
