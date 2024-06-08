78c0695d1f6b34c21690345ff13882b297e3ec46
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-07 22:54:00 -0500

    Working through the fetch request in JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
 const apiKey = '18e7ae3a1f608e812d465476189f028c'
 
 const baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`
+
+function retrieveApiDetails() {
+    fetch(baseUrl) {
+        if(!response.ok) {
+            alert(`Url is not working, response: ${response}`)
+        }
+    }
+}
+retrieveApiDetails()
