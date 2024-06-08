15d777f5b84da19dafc7df96e1eb092efd87fb4f
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-08 11:52:31 -0500

    Working through getting user input to display to the screen as a recent search

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 const apiKey = '18e7ae3a1f608e812d465476189f028c'
 const latitude = '47.6062'
 const longitude = '-122.3321'
-const searchedCity = document.body.getAttribute('city-search')
+const searchedCity = document.body.getAttribute('#city-search')
 
-console.log(searchedCity)
+// console.log(searchedCity)
 
 // create localstorage set up for recent searches so they populate in the aside section of the webpage 
 // localStorage.getItem('searched-city')
 // localStorage.setItem('searched-city', searchedCity.value)
 
-console.log(searchedCity.value)
+// console.log(searchedCity.value)
 
 
 
@@ -32,9 +32,16 @@ const baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude
 // 2. Parse the JSON response.
 // 3. Improve error handling.
 
-const newRecentSearch = document.createElement('div')
-newRecentSearch.textContent = searchedCity.value
 
-document.appendChild(newRecentSearch)
 
-console.log(newRecentSearch)
+// function will create a new div for a recently searched city 
+
+function NewSearchedCity(searchedCity) {
+    const newRecentSearch = document.createElement("div")
+
+    newRecentSearch.textContent = searchedCity.value
+    
+    document.appendChild(newRecentSearch)
+    
+    console.log(newRecentSearch)
+}
